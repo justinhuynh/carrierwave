fce906c07cd53a3e58000c852b162d33aae4b9a7
justinhuynh <jhuynh@gmail.com>
2016-04-11 15:24:20 -0400

    clinic start

--- app/uploaders/profile_photo_uploader.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-class ProfilePhotoUploader < CarrierWave::Uploader::Base
-  storage :file
-  # storage :fog
-  def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-  end
-end
